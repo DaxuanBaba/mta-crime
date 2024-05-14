7e7ae6348ea0d9c54005d2d5f6a623421cad661e
CS9490 <cristianstatescu.cs9490@gmail.com>
2024-05-13 20:44:15 -0400

    added in the actual code for the cleaning/modification to allow file use in d3

--- data/cleaning_for_d3_ridershipdailystationID.py
+++ data/cleaning_for_d3_ridershipdailystationID.py
@@ -1,14 +1,18 @@
 import re
+import pandas as pd
 
 def extract_lat_long(geo_point):
     match = re.search(r'\(([^)]+)', geo_point)
     if match:
         lat_long = match.group(1).split()
         return [float(lat_long[1]), float(lat_long[0])]  # [latitude, longitude]
-    return [None, None]  # Return None if no match found
+    return [None, None] 
 
+data = pd.read_csv('ridership_daily_stationID.csv')
+
+data['Georeference'] = data['Georeference'].astype(str)
 data['coordinates'] = data['Georeference'].apply(extract_lat_long)
 
 map_data = data[['station_complex', 'borough', 'coordinates']]
 
-map_data.to_csv(output_file_path, index=False)
+map_data.to_csv('station_map_data.csv', index=False)
